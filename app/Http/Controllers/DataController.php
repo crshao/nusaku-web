d48dc8989c2b8fc128f1f14476c8acc09d39b3b4
hao <calvindon08@gmail.com>
2020-08-14 20:37:30 +0700

    getDestinationType

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Destination;
+use App\DestinationType;
+use App\User;
 
 class DataController extends Controller
 {
-    public function getDestination()
+    public function getDestinationType()
     {
-        
-        return ;
+        $data = DestinationType::get();
+        foreach($data as $key => $value)
+        {
+            $data = DestinationType::get();
+        }
+        return response()->json($data, 200);
     }
 }
